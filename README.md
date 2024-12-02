05053c05db4384b75d5c663110c62a87bca92dfb
Michał Chmurkowski <4909359+mchmurkowski@users.noreply.github.com>
2024-12-02 20:56:46 +0100

    day 2 completed

--- README.md
+++ README.md
@@ -2,4 +2,5 @@ My solutions to [Advent of Code 2024](https://adventofcode.com/).
 
 > Advent of Code is an Advent calendar of small programming puzzles for a variety of skill levels that can be solved in any programming language you like. People use them as interview prep, company training, university coursework, practice problems, a speed contest, or to challenge each other.
 
-- [Day 1: Historian Hysteria](https://adventofcode.com/2024/day/1) - [solution](day1-historian-hysteria/historians_hysteria.py)
+- [Day 1: Historian Hysteria](https://adventofcode.com/2024/day/1) - [solution](day1-historian-hysteria/historian_hysteria.py)
+- [Day 2: Red-Nosed Reports](https://adventofcode.com/2024/day/2) - [solution](day2-red-nosed-reports/red_nosed_reports.py)
--- day2-red-nosed-reports/red_nosed_reports.py
+++ day2-red-nosed-reports/red_nosed_reports.py
@@ -24,10 +24,23 @@ def is_safe(report: list):
         return True
 
 
+def check_for_dampner(report: list):
+    for x in range(len(report)):
+        report_poped: list = report.copy()
+        report_poped.pop(x)
+        if is_safe(report_poped):
+            return True
+
+
 if __name__ == "__main__":
-    test_cases = process_input("puzzle_input")
+    test_cases: list[list[int]] = process_input("puzzle_input")
     number_of_safe: int = 0
+    number_of_safe_by_dampner: int = 0
     for case in test_cases:
         if is_safe(case):
             number_of_safe += 1
+        else:
+            if check_for_dampner(case):
+                number_of_safe_by_dampner += 1
     print("Answer to part one:", number_of_safe)
+    print("Answer to part two:", number_of_safe + number_of_safe_by_dampner)
